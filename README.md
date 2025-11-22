53c41d1483570edecd6eebf6ac73457ad04f0c8b
Aparna1567 <aparnaapar99@gmail.com>
2025-11-22 13:10:09 +0530

    updated successfully

---
+++ README.md
@@ -0,0 +1,19 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+"Revamped outdated workflows and documentation processes, introducing modern tracking systems, which improved project efficiency and contibuted to 99% client satisfaction.",
+        "Led technical problem solving sessions and directed multi-phase project initiatives, optimizing resource allocation and task scheduling, which boosted project profitability by 50%.",
+        "Designed innovative solutions and operational frameworks to address recurring project challenges leading to 25% faster project turnaround times and improved overall team performance.",
+        "Analyzed project data, performance reports, risk factors and potential bottlenecks across multiple projects, identifying areas for improvement and implementing strategies that reduced project delays by 40%.",
+        "Collaborated with vendors and suppliers to streamline procurement, reducing costs by 20%. "        
---
+++ src/constants/index.js
@@ -0,0 +1,312 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Quick AI",
+    description:
+      "Quick AI is a unified AI workspace that empowers users to generate stunning images, craft engaging articles, remove objects from visuals and review resume seamlessly",
+    subDescription: [
+      "Built a fully functional AI SaaS application using the PERN stack (PostgreSQL, Express, React, Node.js) with secure authentication and profile management powered by Clerk.",
+      "Integrated subscription billing to control access to premium AI features, ensuring scalable and production-ready monetization.",
+      "Designed and optimized a serverless Neon PostgreSQL database, implementing efficient queries and clean API architecture for high performance.",
+      "Developed multiple AI-powered tools including article generator, blog title generator, image generator, background remover, object remover, and resume analyzer."
+    ],
+    href: "https://quick-ai-iota-blond.vercel.app/",
+    logo: "",
+    image: "/assets/projects/quickimg.png",
+    tags: [
+      {
+        id: 1,
+        name: "Express",
+        path: "/assets/logos/f.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "PostgreSQL",
+        path: "/assets/logos/g.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 5,
+        name: "Clerk",
+        path: "/assets/logos/clerk.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Signalist- Real Time Stock Market App with AI Insights",
+    description:
+      "Signalist is a real-time stock market app delivering instant data and AI-driven insights for smarter investing.”",
+    subDescription: [
+        "Built a real-time stock market platform using Next.js, delivering live price updates, fast rendering, and optimized performance.",
+        "Implemented Better Auth for secure user authentication and role-based access, ensuring reliable account protection.",
+        "Integrated interactive stock charts, search functionality, and personalized watchlists for a smooth and intuitive user experience.",
+        "Added AI-powered insights and daily news summaries to help users make informed investment decisions.",
+        "Used Inngest for background jobs and Coderabbit automation for continuous integration and workflow automation, ensuring scalability and reliability."
+    ],
+    href: "https://signalist-ochre.vercel.app/",
+    logo: "",
+    image: "/assets/projects/stock.png",
+    tags: [
+      {
+        id: 1,
+        name: "CodeRabbit",
+        path: "/assets/logos/c.png",
+      },
+      {
+        id: 2,
+        name: "NextJS",
+        path: "/assets/logos/z.png",
+      },
+      {
+        id: 3,
+        name: "Typescript",
+        path: "/assets/logos/b.png",
+      },
+      {
+        id: 4,
+        name: "Better Auth",
+        path: "/assets/logos/better-auth.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Tourvisto - Travel Agency Dashboard",
+    description:
+      "Tourvisto is an advanced travel agency dashboard designed to streamline booking management, monitor customer interactions and deliver insightful analytics- all in one platform",
+    subDescription: [
+      "Built a dynamic Travel Dashboard using React Router v7, featuring smooth navigation and optimized routing performance.",
+      "Integrated Appwrite for secure authentication, database management, and real-time travel data handling.",
+      "Implemented Syncfusion charts & data tables to visualize travel stats, trip history, budgets, and personalized suggestions.",
+      "Added personalized travel recommendations powered by user data and interactive UI components.",
+      "Integrated Sentry for real-time error tracking and monitoring, ensuring stability and production-grade reliability."
+    ],
+    href: "https://tourvistoweb.vercel.app/",
+    logo: "",
+    image: "/assets/projects/tour.png",
+    tags: [
+      {
+        id: 1,
+        name: "Appwrite",
+        path: "/assets/logos/app.png",
+      },
+      {
+        id: 2,
+        name: "Syncfusion",
+        path: "/assets/logos/sync.png",
+      },
+      {
+        id: 3,
+        name: "Sentry",
+        path: "/assets/logos/sen.png",
+      },
+      {
+        id: 4,
+        name: "React Router V7",
+        path: "/assets/logos/reactrouter.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Streamify - Real Time Chat & Video Communication App",
+    description:
+      "Streamify is a modern real time chat application that enables seamless messaging, friend request, video calls with screen sharing, expressive emoji support - all secured with JWT based authentication and designed with a sleek, customizable interface. ",
+    subDescription: [
+      "Developed a full-stack communication platform using Node.js, Express, MongoDB, React, TanStack Query, and Tailwind CSS.",
+      " Implemented secure JWT authentication, onboarding flow, and protected routes for robust user access control.",
+      "Built real-time chat (WebSockets) and peer-to-peer video calling (WebRTC) optimized for low latency and performance.",
+      "Designed a friends system with efficient MongoDB queries and responsive UI interactions.",
+      "Added 32 customizable UI themes and reusable custom hooks to ensure maintainability and enhanced user experience.",
+      "Improved performance and scalability through optimized API design, database indexing, and TanStack Query caching. "
+    ],
+    href: "https://streamify-video-bm8q.onrender.com/",
+    logo: "",
+    image: "/assets/projects/Streamify.png",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDB",
+        path: "/assets/logos/mon.png",
+      },
+      {
+        id: 2,
+        name: "Express",
+        path: "/assets/logos/f.png",
+      },
+      {
+        id: 3,
+        name: "TanStack Query",
+        path: "/assets/logos/tanstack.png",
+      },
+      {
+        id: 4,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+    ],
+  },
+   {
+    id: 5,
+    title: "Awwwards-Inspired Interactive Website — React, GSAP & ScrollTrigger",
+    description:
+      "A visually rich, Awwwards-inspired interactive website crafted with smooth animations, modern UI, and immersive user experiences",
+    subDescription: [
+    "Built a high-performance Awwwards-style interactive website using React, Vite, Tailwind CSS, focusing on premium design and smooth UX.",
+    "Implemented advanced GSAP animations and ScrollTrigger effects for immersive, fluid motion and scroll-based interactions.",
+    "Designed a fully responsive, pixel-perfect layout with optimized assets and modern UI transitions.",
+    "Leveraged Vite for fast development builds and production optimization, ensuring excellent performance and scalability."
+    ],
+    href: "https://awwwardsproj.vercel.app/",
+    logo: "",
+    image: "/assets/projects/awwwards.png",
+    tags: [
+      {
+        id: 1,
+        name: "Appwrite",
+        path: "/assets/logos/app.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Vite",
+        path: "/assets/logos/vitejs.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+     
+    ],
+  },
+  {
+    id: 6,
+    title: "Axora — Modern Business Websitee",
+    description:
+      "Axora is a sleek, modern business website designed with a clean UI, smooth interactions, and a professional brand-focused experience.”",
+    subDescription: [
+    "Built a modern, multi-section business website using Next.js for fast performance and SEO-friendly rendering.",
+    "Designed a clean, responsive UI with Tailwind CSS, suitable for any professional business or agency.",
+    "Implemented reusable components for sections like services, testimonials, pricing, FAQs, and contact forms.",
+    "Ensured excellent performance, accessibility, and deployment readiness with optimized images, routing, and metadata handling."
+    ],
+    href: "https://businesswebsite-9xn4.vercel.app/",
+    logo: "",
+    image: "/assets/projects/busi.png",
+    tags: [
+     {
+        id: 1,
+        name: "NextJS",
+        path: "/assets/logos/z.png",
+      },
+      {
+        id: 2,
+        name: "Typescript",
+        path: "/assets/logos/b.png",
+      },
+     
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+     
+    ],
+  },
+  
+];
+
+export const mySocials = [
+  
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/aparnak1567/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/Aparna1567/My-Projects",
+    icon: "/assets/socials/g.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Project Engineer",
+    job: "Wipro",
+    date: "2022-2023",
+    contents: [
+        "Revamped outdated workflows and documentation processes, introducing modern tracking systems, which improved project efficiency and contibuted to 99% client satisfaction.",
+        "Led technical problem solving sessions and directed multi-phase project initiatives, optimizing resource allocation and task scheduling, which boosted project profitability by 50%.",
+        "Designed innovative solutions and operational frameworks to address recurring project challenges leading to 25% faster project turnaround times and improved overall team performance.",
+        "Analyzed project data, performance reports, risk factors and potential bottlenecks across multiple projects, identifying areas for improvement and implementing strategies that reduced project delays by 40%.",
+        "Collaborated with vendors and suppliers to streamline procurement, reducing costs by 20%. "        
+    ],
+  },
+  
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
